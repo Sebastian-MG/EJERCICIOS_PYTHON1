427fe4e481242f34f3374c5e2fbf3712a85fd182
Sebastian-MG <sebastianmancera12@gmail.com>
2020-03-04 10:44:15 -0500

    .

---
+++ 82_Binario.py
@@ -0,0 +1,9 @@
+# Crear un módulo de funciones aritméticas que realicen las operaciones de suma, resta, multiplicación, división y potencia de enteros, escribir un programa que importe este módulo y use estas funciones para operar con números capturados desde el teclado.
+def sum(a,b):
+    return int(a,2) + int(b,2)
+def resta(a,b):
+    return int(a,2) - int(b,2)
+def div(a,b):
+    return int(a,2)/int(b,2)
+def multi(a,b):
+    return int(a,2)*int(b,2)

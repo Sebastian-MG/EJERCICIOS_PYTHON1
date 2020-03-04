1f80fadb3f2bfc654ca1ec3d6f64fb706a85f25c
Sebastian-MG <sebastianmancera12@gmail.com>
2020-03-04 09:12:57 -0500

    .

---
+++ 21_semana.py
@@ -0,0 +1,18 @@
+# 21. Escribir un programa que dado un número del 1 a 7 escriba el correspondiente nombre del
+# día de la semana.
+
+dia = int(input("Digite el numero del 1 al 7:\n"))
+if dia == 1:
+    print("Lunes")
+elif dia == 2:
+    print("Martes")
+elif dia == 3:
+    print("Miercoles")
+elif dia == 4:
+    print("Jueves")
+elif dia == 5:
+    print("Viernes")
+elif dia == 6:
+    print("Sabado")
+elif dia == 7:
+    print("Domingo")

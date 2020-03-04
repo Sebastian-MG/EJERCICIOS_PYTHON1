e6a721412c1bea3d8f8fac78026ba29ae8da379d
Sebastian-MG <sebastianmancera12@gmail.com>
2020-03-04 09:15:25 -0500

    .

---
+++ 7_esfera.py
@@ -0,0 +1,9 @@
+# 7 Escribir un programa que calcule el volumen de una esfera de radio = r
+# volumen de la esfera = 4/3 * PI * radio3
+
+pi = 3.1416
+radio = int(input("Digite el radio en metros" + '\n'))
+Volumen = int((4 * pi * radio**3) / 3)
+print ("Volumen " + str(Volumen) + " m^3")
+
+
